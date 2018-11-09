3f813560f3847ab813d955984f799cdc11fad24b
vini23hot <vini23hot@gmail.com>
2018-11-09 19:13:14 -0200

    Updated app to add router

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import logo from '../../assets/imgs/logo2.png'
 import './Logo.css'
 
 export default props => (
   <aside className="logo">
-    <a href="/" className="logo">
-    <img src={logo} alt="Logo"/>
-    </a>
+    <Link to="/" className="logo">
+      <img src={logo} alt="Logo"/>
+    </Link>
   </aside>
 )
--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './Nav.css'
 
@@ -6,12 +7,12 @@ export default props => (
   <aside className="menu-area">
     <nav className="menu">
       {/* Refatorar!!! */}
-      <a href="#/">
+      <Link to="/">
         <i className="fa fa-home"></i> Início
-      </a>
-      <a href="#/users">
+      </Link>
+      <Link to="/users">
         <i className="fa fa-users"></i> Usuários
-      </a>
+      </Link>
     </nav>
   </aside>
 )
